51959b8cb545eddbaf8e8ed6c93f9822178279cf
santsbabe <santsbabe@gmail.com>
2026-09-11 17:39:06 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE='bloom-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+
+self.addEventListener('install',e=>
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS))
+  )
+);
+
+self.addEventListener('activate',e=>
+  e.waitUntil(
+    caches.keys().then(keys=>
+      Promise.all(
+        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+      )
+    )
+  )
+);
+
+self.addEventListener('fetch',e=>
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  )
+);
