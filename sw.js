d389f5ad287a6ca5b7dbaefe1a396c2121721fdf
santsbabe <santsbabe@gmail.com>
2026-09-13 18:20:07 +0200

    Refresh Bloom offline cache for UI and insights

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='bloom-v7';
-const ASSETS=['./','./index.html','./ui.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='bloom-v8';
+const ASSETS=['./','./index.html','./ui.css','./app.js','./insights.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
