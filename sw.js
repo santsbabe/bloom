76cec30fd37387c9ed86af1e8a080ee152250740
santsbabe <santsbabe@gmail.com>
2026-09-13 18:58:39 +0200

    Refresh Bloom cache for Today flow and cycle symptoms

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bloom-v9';
+const CACHE='bloom-v10';
 const ASSETS=['./','./index.html','./ui.css','./app.js','./insights.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
