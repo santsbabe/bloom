5ea819174fb74efbc11d92c976d151249a2c06d0
santsbabe <santsbabe@gmail.com>
2026-09-25 19:57:35 +0200

    Add captions to visual trigger icons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bloom-v32';
+const CACHE='bloom-v33';
 const ASSETS=['./','./index.html','./styles.css','./trigger-assets.css','./trigger-interactions.css','./baseline-overlay.css','./design-v32.css','./app.js','./manifest.webmanifest','./bloom.config.json','./assets/lotus-check-in.webp','./assets/triggers/trigger-assets.json','./assets/triggers/sleep.png','./assets/triggers/caffeinate.png','./assets/triggers/alcohol.png','./assets/triggers/breathe.png','./assets/triggers/hydrate.png','./assets/triggers/sense.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
